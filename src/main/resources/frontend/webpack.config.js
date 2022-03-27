81f03c8ee85cc2b8e803dad2e479d931ea00efd0
엄태권 <ultrataekwan@real-iv.co.kr>
2022-03-27 11:22:56 +0900

    frontend build

--- src/main/resources/frontend/webpack.config.js
+++ src/main/resources/frontend/webpack.config.js
@@ -19,7 +19,7 @@ module.exports ={
             loader: 'babel-loader',
             options:{
                 presets: ['@babel/preset-env', '@babel/preset-react'],
-                // plugins:['react-refresh/babel'],
+                // plugins:['react-refresh/babel'], //build시 빼야함.
             }},
             { test: /\.css$/, use:['css-loader']},
         ],
